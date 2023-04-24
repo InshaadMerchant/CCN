8c8d4f88c2a5722fc8e2e01b07f256623a826f80
Athrva Arora <aroraathrva1@gmail.com>
2023-04-24 01:15:37 -0500

    Pushed Login and Registration changes

--- app/src/main/java/com/example/ccn/Registration_1.java
+++ app/src/main/java/com/example/ccn/Registration_1.java
@@ -7,14 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Registration_1 extends AppCompatActivity {
-    TextInputEditText regName, regUsername, regEmail, regPassword;
-    Button regButton, regtoLoginButton;
-
+    TextInputEditText regName, regUsername, regEmail, regPassword, security_answer;
+    Button regNextButton, regtoLoginButton;
+    private Spinner Security_Question;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +32,30 @@ public class Registration_1 extends AppCompatActivity {
         regUsername = findViewById(R.id.reg_username);
         regEmail = findViewById(R.id.reg_email);
         regPassword = findViewById(R.id.reg_password);
-        regtoLoginButton= findViewById(R.id.existing_user);
+        regtoLoginButton = findViewById(R.id.existing_user);
+        Security_Question = findViewById(R.id.secutity_Question);
+        String[] Security_Questions = getResources().getStringArray(R.array.Security_Question);
+        ArrayAdapter adapter = new ArrayAdapter( this, android.R.layout.simple_spinner_item,Security_Questions);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Security_Question.setAdapter(adapter);
+        security_answer = findViewById(R.id.secutity_Question_answer);
+        regNextButton = findViewById(R.id.Next_Button);
+        String security_question = Security_Question.getSelectedItem().toString();
+
         regtoLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (Registration_1.this,Login.class);
                 startActivity(intent);
+            }
+        });
+        regNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Registration Info");
+                reference.setValue("");
+
             }
         });
     }
