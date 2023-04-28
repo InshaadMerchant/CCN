9cc12d8916cf9b58cf1eb25074627a77590c6102
InshaadMerchant <114313560+InshaadMerchant@users.noreply.github.com>
2023-04-27 23:05:03 -0500

    navigation_bar

--- app/src/main/java/com/example/ccn/MainActivity.java
+++ app/src/main/java/com/example/ccn/MainActivity.java
@@ -16,80 +16,11 @@ import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    ActionBarDrawerToggle drawerToggle;
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if (drawerToggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item)
-            {
-                switch(item.getItemId())
-                {
-                    case R.id.home:
-                    {
-                        Toast.makeText(MainActivity.this, "home selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    }
-                    case R.id.add_post:
-                    {
-                        Toast.makeText(MainActivity.this, "Add Post Selected", Toast.LENGTH_SHORT).show();
-                        //Intent intent = new Intent(MainActivity.this, creating_post.class);
-                        //startActivity(intent);
-                        break;
-                    }
-                    case R.id.about:
-                    {
-                        Toast.makeText(MainActivity.this, "about selected", Toast.LENGTH_SHORT).show();
-                        //Intent intent = new Intent(MainActivity.this, profile.class);
-                        //startActivity(intent);
-                        break;
-                    }
-                    case R.id.logout:
-                    {
-                        Toast.makeText(MainActivity.this, "logout selected", Toast.LENGTH_SHORT).show();
-                        //Intent intent = new Intent(MainActivity.this, login.class);
-                        //startActivity(intent);
-                        break;
-                    }
-                    case R.id.share:
-                    {
-                        Toast.makeText(MainActivity.this, "share selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    }
-                }
-                return false;
-            }
-        });
-    }
-    @Override
-    public void onBackPressed(){
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
-        {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
-        {
-            super.onBackPressed();
-        }
     }
 }
--- app/src/main/java/com/example/ccn/post_button.java
+++ app/src/main/java/com/example/ccn/post_button.java
@@ -1,11 +1,14 @@
+package com.example.ccn;
+
 import static androidx.core.content.ContextCompat.startActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import com.example.ccn.MainActivity;
-import com.example.ccn.R;
 public class post_button
 {
     private ImageButton post_button;
